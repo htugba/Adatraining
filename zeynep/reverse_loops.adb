158ef097c98a184b7bc8c195b2d5fa8e048852a9
Zeynep Oguz <34620331+zeynepoguz@users.noreply.github.com>
2020-05-02 15:22:00 +0300

    Delete reverse_loops.adb

--- zeynep/reverse_loops.adb
+++
@@ -1,8 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-procedure Reverse_Loops is
-   begin
-      for I in reverse 1 .. 8 loop
-         Put_Line("Counting .. " & Integer'Image(I) );
-      end loop;
-   end Reverse_Loops;
